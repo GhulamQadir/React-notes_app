eda73550e4ef40710e7c5059f9053e1279d94449
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2022-09-13 13:03:09 -0700

    little bit ui updated

--- src/Components/render_todos/render-notes.js
+++ src/Components/render_todos/render-notes.js
@@ -1,71 +1,16 @@
-// import { withRouter } from "react-router-dom";
 import { useHistory } from 'react-router-dom';
 import './render-notes.css'
 
 
 
 
-// class RenderNotes extends Component {
-
-
-
-
-//     deleteTodo(index) {
-//         let { notes } = this.props.state
-//         notes.splice(index, 1)
-//         this.setState({
-//             notes: notes
-//         })
-//     }
-
-
-
-//     viewNote = (index) => {
-//         let { updatedInputValues, notes } = this.props.state
-//         let note = this.props.state.notes[index]
-//         this.props.history.push({ pathname: '/note-details', state: { note: note, index: index, notes: notes, updatedInputValues: updatedInputValues } })
-//     }
-
-
-//     componentDidUpdate = () => {
-//         localStorage.setItem('notes', JSON.stringify(this.props.state.notes))
-//     }
-
-
-//     render() {
-//         return (
-//             <div>
-//                 <h1>Render Notes component</h1>
-
-//                 <div className="notes_main_div">
-//                     {this.props.state.notes && this.props.state.notes.map((note, index) => {
-//                         return <div className="note_div" key={index}>
-//                             {note.isImportantNote ? <p>Important !</p> : ""}
-//                             <br />
-//                             <br />
-//                             <li style={{ fontWeight: "bold" }}>{index + 1}: </li>
-//                             <li>{note.isEdit ? <input onChange={(e) => this.editTodoValueOnchange(e)} defaultValue={note.title} type="text" /> : note.title}</li>
-//                             <li>{note.isEdit ? <button onClick={() => this.cancel(index)}>Cancel</button> : <button onClick={() => this.deleteTodo(index)}>Delete</button>}</li>
-
-//                             <br />
-//                             <br />
-//                             <button onClick={() => this.viewNote(index)}>View Note</button>
-//                         </div>
-//                     })}
-//                 </div>
-//             </div >
-//         )
-//     }
-// }
-
-// export default withRouter(RenderNotes);
 
 
 function RenderNotes(props) {
     const history = useHistory();
     let { notes } = props
 
-    const deleteNote = (index) => {
+    const removeNote = (index) => {
         notes.splice(index, 1)
         console.log(notes)
         notes = notes
@@ -79,23 +24,24 @@ function RenderNotes(props) {
         history.push({ pathname: '/note-details', state: { note: note, index: index, notes: notes, updatedInputValues: updatedInputValues } })
     }
 
+
     return (
         <div>
             <h1>Render Notes component</h1>
 
             <div className="notes_main_div">
                 {props.notes && props.notes.map((note, index) => {
-                    return <div className="note_div" key={index}>
-                        {note.isImportantNote ? <p>Important !</p> : ""}
-                        <br />
-                        <br />
-                        <h2>{note.title}</h2>
+                    return <div className="note" key={index}>
+                        {/* {note.isImportantNote ? <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJltZ_46zMjqNlTMb9MlfLgOEH_DO5XbzZmvMe5m5gdzlAFlHIgZtXPT-jv58kv95o4Ew&usqp=CAU' height="30" /> : ""} */}
+                        <button className="remove_note_btn" onClick={() => removeNote(index)}><i className="fa fa-times"></i></button>
+                        {/* <br />
+                        <br /> */}
+                        <p className="note_title">{note.title}</p>
 
-                        <button onClick={() => deleteNote(index)}>Delete</button>
 
                         <br />
                         <br />
-                        <button onClick={() => viewNote(index)}>View Note</button>
+                        {/* <button onClick={() => viewNote(index)}>View Note</button> */}
                     </div>
                 })}
             </div>
